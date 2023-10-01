75e4f8200b1a11dedf4d375ffb9726cdfc5c8f6b
Salikhov Dmitriy <salikhov@dikom.ru>
2023-10-01 20:04:51 +0300

    Test update

--- .idea/src/index.php
+++ .idea/src/index.php
@@ -1,2 +1,14 @@
 <?php
 echo "Hello world!";
+?>
+
+<form method=POST>
+    <input type="submit" name="button" value="Click me!" />
+</form>
+
+<?php
+    if (isset ($_POST['button']))
+    {
+        echo "Анька писька";
+    }
+?>
